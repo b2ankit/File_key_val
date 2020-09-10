473a51e087f3c8796e6f81a3d688c4cb04303781
b2ankit <ankit15ec007@gmail.com>
2020-09-10 18:27:40 +0530

    fixed1

--- main.c
+++ main.c
@@ -33,11 +33,15 @@ int file_read(char *key,char *valuetoWrite){
     int retVal = 0;
     int keyLen = strlen(key)+1;
     int vaLen;
-    int sizetoRead = 128;
+    int sizetoRead = 264;
     char *readData = NULL;
+    char *modified_key = NULL;
+    char *line_holder = NULL;
+    int modified_keyLen = strlen(key)+1;
 
     FILE *file_pointer = NULL;
     file_pointer = mul_fs_fopen(FILEPATH,"r+");
+
 
     readData = (char *)calloc(sizetoRead,sizeof(char));
     if(!readData){
@@ -45,15 +49,24 @@ int file_read(char *key,char *valuetoWrite){
         return -1;
     }
 
-    char *line_holder = NULL;
-    line_holder = (char *)calloc(64,sizeof(char));
+    modified_key = (char *)calloc(modified_keyLen,sizeof(char));
+    if(!modified_key){
+        printf("Memory allocation for modified_key : Failed\n");
+        return -1;
+    }
+    sprintf(modified_key,"%s%s",key,"=");
+
+    printf("Modified key value : %s\n",modified_key);
+
+
+    line_holder = (char *)calloc(128,sizeof(char));
     if(!line_holder){
         printf("memory allocation for line_holder : Failed\n");
         return -1;
     }
 
     if(file_pointer){
-        retVal = mul_fs_read_key(file_pointer,readData,sizetoRead,key,line_holder);
+        retVal = mul_fs_read_key(file_pointer,readData,sizetoRead,modified_key,line_holder);
         if(retVal!=1)
             return -1;
         vaLen = strlen(line_holder)-(keyLen+1);
@@ -75,10 +88,10 @@ int main(){
 
     /*Writing in file*/
     file_write("Hello","world");
-    file_write("Avana","Jio");
-    file_write("Ankit","Kumar");
+    file_write("Ankit","Jioka naam");
     file_write("office","jioooo");
     file_write("address","chapra");
+    file_write("Ankit1","Kumar");
 
     /*Reading in file*/
     
